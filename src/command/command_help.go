070203b7c5e1ff1f28561e889eab7bc7be591d20
Yan Justino <yanlimaj@gmail.com>
2020-08-09 20:54:41 -0300

    help command

---
+++ src/command/command_help.go
@@ -0,0 +1,56 @@
+package command
+
+type HelpArgs struct {
+	Args []string
+}
+
+/* list -k {{file}} */
+func (args HelpArgs) CanExecute() bool {
+	param := args.Args[1:]
+
+	return len(args.Args) == 1 ||
+		param[0] == "help" || param[0] == "-h" || param[0] == "--h"
+}
+
+func (args HelpArgs) Execute() (bool, error) {
+	if !args.CanExecute() {
+		return false, nil
+	}
+	println(AsciiBanner())
+	println(Helptext())
+	return true, nil
+}
+
+func AsciiBanner() string {
+	return " ______ _      _      _____  _____\n" +
+		"|  ____| |    | |    |_   _|/ ____|\n" +
+		"| |__  | |    | |      | | | (___\n" +
+		"|  __| | |    | |      | |  \\___ \\\n" +
+		"| |____| |____| |____ _| |_ ____) |\n" +
+		"|______|______|______|_____|_____/\n\n"
+}
+
+func Helptext() string {
+	return " Usage: ellis <command> [options] [path-to-jwk]\n" +
+		" Usage: ellis [path-to-jwk]\n" +
+		" \n" +
+		" command:\n" +
+		"   help  Display help \n" +
+		"   keys  Generate a pair of RSA Keys (The public key is generated in JWK format) \n" +
+		"   list  List all secrets for JWK file \n" +
+		"   view  Preview the settings file \n" +
+		"   set   Store a key-value pair \n" +
+		"   eject Generate a settings file \n" +
+		" \n" +
+		" command [options]:\n" +
+		"   help  [-h] \n" +
+		"   keys  [-g] [jwk-id] \n" +
+		"   list  [-k] [path-to-jwk] \n" +
+		"   view  [-k] [path-to-jwk] \n" +
+		"   set   [-k] [path-to-jwk] [key] [value]\n" +
+		"   eject [-k] [path-to-jwk] \n" +
+		" \n" +
+		"path-to-jwk:\n" +
+		" 	The path to an application JWK file to execute.\n"
+
+}
--- src/ellis.go
+++ src/ellis.go
@@ -1,3 +1,26 @@
+/*
+MIT License
+Data: 05/08/2020
+Copyright (c) 2020 Yan Justino
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 package main
 
 import (
@@ -6,9 +29,15 @@ import (
 )
 
 func main() {
-	command.KeyGenerateArgs{Args: os.Args}.Execute()
-	command.SetSecretArgs{Args: os.Args}.Execute()
-	command.ListSecretsArgs{Args: os.Args}.Execute()
-	command.ViewSecretsArgs{Args: os.Args}.Execute()
-	command.EjectSecretsArgs{Args: os.Args}.Execute()
+	var e error
+	_, e = command.KeyGenerateArgs{Args: os.Args}.Execute()
+	_, e = command.SetSecretArgs{Args: os.Args}.Execute()
+	_, e = command.ListSecretsArgs{Args: os.Args}.Execute()
+	_, e = command.ViewSecretsArgs{Args: os.Args}.Execute()
+	_, e = command.EjectSecretsArgs{Args: os.Args}.Execute()
+	_, e = command.HelpArgs{Args: os.Args}.Execute()
+
+	if e != nil {
+		println(e.Error())
+	}
 }
